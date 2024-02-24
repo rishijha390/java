44634227feb6e7ffbabdc2a2935a0bdfb16cc767
rishijha390 <160853195+rishijha390@users.noreply.github.com>
2024-02-24 14:21:21 +0530

    Add files via upload

---
+++ Cwh_15_ps.java
@@ -0,0 +1,33 @@
+public class Cwh_15_ps{
+    public static void main(String args[]){
+        String name ="Ri sh  i Jha";
+        //problem 1
+        /* name=name.toLowerCase();
+        System.out.println(name); */
+        //problem 2
+        name=name.replace(' ','_');
+        System.out.println(name);
+        //problem 3
+     /*    String letter ="Dear <|name|>,Thanks a lot!";
+        letter=letter.replace("<|name|>","Golu");
+        System.out.println(letter); */
+       //problem-4
+       /*  String letter ="Hey everyone! hope all   of you are well";
+       
+        System.out.println(letter.indexOf(" "));
+        System.out.println(letter.indexOf("   ")); */
+
+
+        //problem 5
+        String transformer="Dear Harry, \n\tThis Java Course is Nice.\n\tThanks!";
+        System.out.println(transformer);
+
+
+        
+        
+        
+
+        
+
+    }
+}
